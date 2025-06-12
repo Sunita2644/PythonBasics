52826dc606bc5594888b3c184368ea15a70ea1a9
Sunita2644 <sunita.tanawade@yahoo.com>
2025-06-12 15:43:41 +0530

    new exception handling program

--- exceptionHandling.py
+++ exceptionHandling.py
@@ -1,4 +1,4 @@
-
+#example 1
 try:
     with open("testData.txt", 'r') as file:
         data=file.read()
@@ -7,3 +7,18 @@ except Exception as e:
     print(e)
 finally:
     print("test data loaded")
+
+
+#example 2
+test_cases = {101:"login test",
+              102:"logout test"}
+def run_test_case(case_id):
+    try:
+        print(f"test case details:{case_id}-{test_cases[case_id]}")
+    except Exception as e:
+        print(f"Keyerror",e)
+    finally:
+        print("checked")
+
+run_test_case(101)
+run_test_case(103)
